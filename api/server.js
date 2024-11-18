dc3dbc8c5a2db3bc03880deccf8ff34ca2a0e332
martin <guerreiromartin@gmail.com>
2024-11-17 23:13:57 -0300

    modificaciones en el diseño de la vista y agregado de tareas

--- api/server.js
+++ api/server.js
@@ -1,43 +1,59 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = 3000;
-const cors = require('cors');
-
-
+const cors = require("cors");
 
 app.use(express.json()); // Middleware para interpretar JSON en las solicitudes
 
 app.use(cors()); // middleware para habilitar CORS en server.js
 
-
 // Datos en memoria para almacenar las tareas
-let tasks = [];
+let tasks = [
+  {
+    id: 1,
+    title: "Comprar víveres",
+    description: "Ir al supermercado a comprar víveres.",
+    completed: false,
+  },
+  {
+    id: 2,
+    title: "Limpiar la casa",
+    description: "Limpiar el baño y la cocina.",
+    completed: true,
+  },
+  {
+    id: 3,
+    title: "Hacer ejercicio",
+    description: "Correr 5 km por la mañana.",
+    completed: false,
+  },
+];
 
 // Ruta para crear una nueva tarea
-app.post('/tasks', (req, res) => {
+app.post("/tasks", (req, res) => {
   const newTask = { id: tasks.length + 1, ...req.body, completed: false };
   tasks.push(newTask);
   res.status(201).json(newTask);
 });
 
 // Ruta para listar todas las tareas
-app.get('/tasks', (req, res) => {
+app.get("/tasks", (req, res) => {
   res.json(tasks);
 });
 
 // Ruta para actualizar una tarea por ID
-app.put('/tasks/:id', (req, res) => {
+app.put("/tasks/:id", (req, res) => {
   const taskId = parseInt(req.params.id);
   const taskIndex = tasks.findIndex((t) => t.id === taskId);
-  
-  if (taskIndex === -1) return res.status(404).send('Tarea no encontrada');
-  
+
+  if (taskIndex === -1) return res.status(404).send("Tarea no encontrada");
+
   tasks[taskIndex] = { ...tasks[taskIndex], ...req.body };
   res.json(tasks[taskIndex]);
 });
 
 // Ruta para eliminar una tarea por ID
-app.delete('/tasks/:id', (req, res) => {
+app.delete("/tasks/:id", (req, res) => {
   const taskId = parseInt(req.params.id);
   tasks = tasks.filter((t) => t.id !== taskId);
   res.status(204).send(); // 204 No Content para eliminación exitosa
