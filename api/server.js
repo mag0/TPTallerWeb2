8c62dcf19adfc041a02a95e5788ef146c8155677
martin <guerreiromartin@gmail.com>
2024-12-13 12:25:43 -0300

    arreglos

--- api/server.js
+++ api/server.js
@@ -1,13 +1,10 @@
 const express = require("express");
 const app = express();
-const PORT = 3000;
 const cors = require("cors");
 
-app.use(express.json()); // Middleware para interpretar JSON en las solicitudes
+app.use(express.json());
+app.use(cors());
 
-app.use(cors()); // middleware para habilitar CORS en server.js
-
-// Datos en memoria para almacenar las tareas
 let tasks = [
   {
     id: 1,
@@ -29,19 +26,16 @@ let tasks = [
   },
 ];
 
-// Ruta para crear una nueva tarea
 app.post("/tasks", (req, res) => {
   const newTask = { id: tasks.length + 1, ...req.body, completed: false };
   tasks.push(newTask);
   res.status(201).json(newTask);
 });
 
-// Ruta para listar todas las tareas
 app.get("/tasks", (req, res) => {
   res.json(tasks);
 });
 
-// Ruta para actualizar una tarea por ID
 app.put("/tasks/:id", (req, res) => {
   const taskId = parseInt(req.params.id);
   const taskIndex = tasks.findIndex((t) => t.id === taskId);
@@ -52,14 +46,10 @@ app.put("/tasks/:id", (req, res) => {
   res.json(tasks[taskIndex]);
 });
 
-// Ruta para eliminar una tarea por ID
 app.delete("/tasks/:id", (req, res) => {
   const taskId = parseInt(req.params.id);
   tasks = tasks.filter((t) => t.id !== taskId);
-  res.status(204).send(); // 204 No Content para eliminación exitosa
+  res.status(204).send();
 });
 
-// Iniciar el servidor
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+module.exports = app;
